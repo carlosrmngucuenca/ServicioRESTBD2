9c1202bec86f00eb42ef52644594181ed860a24d
carlosrmngucuenca <carlos.munozg@ucuenca.edu.ec>
2020-07-11 01:16:04 -0500

    Inicio Proyecto

---
+++ Bases2/src/main/java/com/TrabajoFinal/core/Bases2Application.java
@@ -0,0 +1,13 @@
+package com.TrabajoFinal.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bases2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bases2Application.class, args);
+	}
+
+}
